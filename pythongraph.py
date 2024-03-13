384824be6dde441dc61dc23df4830008b95f77fa
Woollends <148427787+Woollends@users.noreply.github.com>
2024-03-13 00:13:43 -0700

    Update pythongraph.py

--- pythongraph.py
+++ pythongraph.py
@@ -5,8 +5,19 @@ def read_serial_data(port, baud_rate):
     ser = serial.Serial(port, baud_rate)  # Set a timeout of 1 second
     
     try:
-        periods = []
+        while True:
+            line = ser.readline().decode().strip()
+            if "fluffy bunnies" in line:
+                print("Received 'fluffy bunnies'. Starting to take readings.")
+                break
 
+    except KeyboardInterrupt:
+        print("Script terminated.")
+        return
+
+    periods = []
+
+    try:
         while True:
             line = ser.readline().decode().strip()
 
@@ -16,9 +27,10 @@ def read_serial_data(port, baud_rate):
             try:
                 period = float(line)
                 periods.append(period)
+                print(f"Received period: {period}")
 
             except ValueError:
-                print(f"{line}")
+                print(f"Invalid value received: {line}")
 
     except KeyboardInterrupt:
         print("Script terminated.")
@@ -35,17 +47,16 @@ def generate_and_show_plot(data):
 
         plt.xlim(min_value, max_value)
         plt.hist(data, bins=20, edgecolor='black')
-        plt.xlabel('Period (ms)')
-        plt.ylabel('Frequency')
+        plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Quantity')
         plt.title('Period Histogram')
         plt.show()
 
 if __name__ == "__main__":
-    serial_port = 'COM8'  # Update with the correct COM port
+    serial_port = 'COM5'  # Update with the correct COM port
     baud_rate = 115200
 
     try:
-        periods_array = []
         read_serial_data(serial_port, baud_rate)
     except Exception as e:
         print(f"An error occurred: {e}")
